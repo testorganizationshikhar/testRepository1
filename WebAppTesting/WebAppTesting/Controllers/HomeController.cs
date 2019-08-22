9ae93a75db1daca81990fc83641bd20ad8c7cf03
shikhar0210 <54311755+shikhar0210@users.noreply.github.com>
2019-08-22 11:35:02 +0530

    Update HomeController.cs

--- WebAppTesting/WebAppTesting/Controllers/HomeController.cs
+++ WebAppTesting/WebAppTesting/Controllers/HomeController.cs
@@ -22,7 +22,8 @@ namespace WebAppTesting.Controllers
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
-        {
+        { 
+        
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
